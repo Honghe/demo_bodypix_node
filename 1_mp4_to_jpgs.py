e5a308e9c007c2ba8088db2c36e7416b849a6aea
Jack Wu <leopardsaga@gmail.com>
2020-01-15 17:24:01 +0800

    Modify. use 25 fps at FFmpeg.

--- 1_mp4_to_jpgs.py
+++ 1_mp4_to_jpgs.py
@@ -9,7 +9,7 @@ def main(root_dir):
     mp4_file = os.path.join(root_dir, '{}.mp4'.format(os.path.basename(root_dir)))
     jpgs_dir = os.path.join(root_dir, 'jpgs')
     os.makedirs(jpgs_dir, exist_ok=True)
-    args = ['ffmpeg', '-y', '-i', mp4_file, '-r', '30', '{}/%4d.jpg'.format(jpgs_dir)]
+    args = ['ffmpeg', '-y', '-i', mp4_file, '-r', '25', '{}/%4d.jpg'.format(jpgs_dir)]
     process = subprocess.Popen(
         args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
     _, stderr = process.communicate()
--- 4_jpgs_to_mp4.py
+++ 4_jpgs_to_mp4.py
@@ -9,7 +9,7 @@ def main(root_dir):
     mp4_file = os.path.join(root_dir, 'masked.mp4')
     jpgs_dir = os.path.join(root_dir, 'masked_jpgs')
     os.makedirs(jpgs_dir, exist_ok=True)
-    args = ['ffmpeg', '-y', '-i', '{}/%4d.jpg'.format(jpgs_dir), '-vcodec', 'libx264', mp4_file]
+    args = ['ffmpeg', '-y', '-i', '{}/%4d.jpg'.format(jpgs_dir), '-vcodec', 'libx264', '-r', '25', mp4_file]
     print(' '.join(args))
     process = subprocess.Popen(
         args, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=False)
